ac9f3d00b22ed735f568d98a01de7dbf26988660
petya8bachey <spbor@yandex.ru>
2023-10-01 19:50:07 +0300

    Add all classes

--- src/Firm.java
+++ src/Firm.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Dictionary;
+
 public class Firm {
+    String name; //Наименование фирмы
+    String country;//Страна
+    String region;//Регион (область)
+    String town;//Город
+    String street;//Улица
+    String postInx;//Почтовый индекс
+    Date dateIn;//Дата ввода фирмы (начало взаимоотношений)
+    String email;//Почтовый адрес фирмы
+    String web;//URL-адрес сайта
+    ArrayList<SubFirm> _sbFirms;//Подразделения фирмы
+    Dictionary<String, String> _usrFields;//Пользовательские поля
+
 }
