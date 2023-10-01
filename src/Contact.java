b186f352a891acb2992a6f0b3fabfd6193c92eb3
petya8bachey <spbor@yandex.ru>
2023-10-01 19:48:07 +0300

    Add all classes

--- src/Contact.java
+++ src/Contact.java
@@ -1,2 +1,10 @@
+import java.util.Date;
+
 public class Contact {
+    Date beginDt;//Дата начала контакта
+    Date endDt;//Дата завершения контакта
+    String descr; //Описание контакта для себя
+    String _dataInfo;//Формулировка контакта для клиента
+    ContType _tpy;//Вид контакта
+
 }
--- src/SubFirm.java
+++ src/SubFirm.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class SubFirm {
+    String name;//Наименование подразделения
+    String bossName;//Имя руководителя подразделения
+    String ofcBossName;//Официальное обращение к руководителю
+    String tel;//номер телефона подразделения
+    String email;//Почтовый адрес подразделения
+    SubFirmType tpy;//Тип подразделения
+    ArrayList<Contact> conts;//Контакты подразделения
+
 }
