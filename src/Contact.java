0b27c7154c3b1291890a91cda407b800d84dd166
petya8bachey <spbor@yandex.ru>
2023-10-01 19:52:05 +0300

    Add all classes

--- src/Contact.java
+++ src/Contact.java
@@ -4,7 +4,7 @@ public class Contact {
     Date beginDt;//Дата начала контакта
     Date endDt;//Дата завершения контакта
     String descr; //Описание контакта для себя
-    String _dataInfo;//Формулировка контакта для клиента
-    ContType _tpy;//Вид контакта
+    String dataInfo;//Формулировка контакта для клиента
+    ContType tpy;//Вид контакта
 
 }
